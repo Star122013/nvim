92b431abb17df323596d96000e465408b35b9323
Kiana <hyy122013@outlook.com>
2026-01-01 20:49:05 +0800

    ✨ Add direnv.nvim

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,3 +1,7 @@
+local direnv = function()
+  return require('direnv').statusline()
+end
+
 return {
   'nvim-lualine/lualine.nvim',
   event = "LazyFile",
@@ -40,7 +44,7 @@ return {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { 'filename' },
-        lualine_x = { 'lsp_status', 'filetype' },
+        lualine_x = { direnv, 'lsp_status', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
       },
---
+++ lua/plugins/utils/direnv.lua
@@ -0,0 +1,36 @@
+return {
+  "NotAShelf/direnv.nvim",
+  event = "LazyFile",
+  config = function()
+    require("direnv").setup({
+      bin = "direnv",
+
+      -- Whether to automatically load direnv when entering a directory with .envrc
+      autoload_direnv = true,
+
+      -- Statusline integration
+      statusline = {
+        -- Enable statusline component
+        enabled = true,
+        -- Icon to display in statusline
+        icon = "󱚟",
+      },
+
+      -- Keyboard mappings
+      keybindings = {
+        allow = "<Leader>dia",
+        deny = "<Leader>did",
+        reload = "<Leader>dir",
+        edit = "<Leader>die",
+      },
+
+      -- Notification settings
+      notifications = {
+        -- Log level (vim.log.levels.INFO, ERROR, etc.)
+        level = vim.log.levels.INFO,
+        -- Don't show notifications during autoload
+        silent_autoload = true,
+      },
+    })
+  end,
+}
